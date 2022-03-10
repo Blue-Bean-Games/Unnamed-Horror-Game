3f1f43114cef888f757f478b63d48d0d0faec4cd
yasserhcn <hacini_yasser@yahoo.fr>
2022-03-10 19:54:30 +0100

    added prone and slower movement when prone/crouch

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,7 +2,7 @@ extends KinematicBody
 
 var current_speed = Vector3(0, 0, 0)
 var jump_vector = Vector3(0, 0, 0)
-var movement_speed = 0.5
+var movement_speed = 50
 var mouse_sensitivity = 0.005
 var gravity = -100
 var sprinting = false
@@ -30,9 +30,9 @@ func get_movement_input():
 		movement_direction += global_transform.basis.x
 	
 	# sprinting
-	if Input.is_action_pressed("sprint"):
+	if Input.is_action_just_pressed("sprint"):
 		movement_speed = 100
-	else:
+	elif Input.is_action_just_released("sprint"):
 		movement_speed = 50
 	
 	movement_direction = movement_direction.normalized()
@@ -50,8 +50,18 @@ func check_keyboard_input():
 	# crouching
 	if Input.is_action_just_pressed("crouch"):
 		$CollisionShape.shape.height = 2
+		movement_speed = 25
 	elif Input.is_action_just_released("crouch"):
 		$CollisionShape.shape.height = 4
+		movement_speed = 50
+	
+	# prone
+	if Input.is_action_just_pressed("prone"):
+		$CollisionShape.rotate_x(deg2rad(90))
+		movement_speed = 10
+	elif Input.is_action_just_released("prone"):
+		$CollisionShape.rotate_x(deg2rad(-90))
+		movement_speed = 50
 
 func _physics_process(delta):
 	check_keyboard_input()
@@ -64,8 +74,6 @@ func _physics_process(delta):
 	# add gravity
 	if not is_on_floor():
 		jump_vector.y += gravity * delta
-	
-	print(current_speed)
 	
 	move_and_slide(current_speed)
 	move_and_slide(jump_vector, Vector3.UP)
